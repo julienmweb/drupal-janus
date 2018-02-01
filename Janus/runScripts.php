9a579603e0bd81a4f7a041e81c05f5f503c69d0e
Julien Marciliac <jmarciliac@mamasam.net>
2018-02-01 14:24:59 +0100

    mise à jour des inclusions de fichiers dans runScripts

--- Janus/runScripts.php
+++ Janus/runScripts.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 \define('NODES_ATTACHED_FILES_FOLDER', __DIR__ . '/Data/AttachedFiles');
 
 ///// Includes your script Files /////
-//include_once __DIR__ . '/Scripts/nodes.php';
+include_once __DIR__ . '/Scripts/nodes.php';
 include_once __DIR__ . '/Scripts/users.php';
-//include_once __DIR__ . '/Scripts/associateExample1.php';
-//include_once __DIR__ . '/Scripts/associateExample2.php';
+include_once __DIR__ . '/Scripts/associateExample1.php';
+include_once __DIR__ . '/Scripts/associateExample2.php';
